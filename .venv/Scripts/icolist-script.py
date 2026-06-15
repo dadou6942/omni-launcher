c692cc635236490065c2b3fdd0b275a1e6948d8f
Dadou <durgettheo@gmail.com>
2026-06-15 19:58:23 +0200

    .gitignore update

---
+++ .venv/Scripts/icolist-script.py
@@ -0,0 +1,12 @@
+#!c:\users\theod\pycharmprojects\omnilauncher\.venv\scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'icoextract==0.1.6','console_scripts','icolist'
+__requires__ = 'icoextract==0.1.6'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('icoextract==0.1.6', 'console_scripts', 'icolist')()
+    )
